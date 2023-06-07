2954e1e26d7bccc434c71a23b92b00f8f6274324
Johnnie Menges Giacomelli <johnniegiacomelli@gmail.com>
2023-06-07 00:06:55 -0300

    feat: Adicionado bloqueio por período

--- site/script.js
+++ site/script.js
@@ -1,160 +1,89 @@
-let diasSemana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"];
-let disciplinas = {};
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('form-disciplina');
+  const tabela = document.getElementById('tabela-disciplinas');
 
-let formDisciplina = document.getElementById("form-disciplina");
-formDisciplina.addEventListener("submit", function(event) {
-  event.preventDefault();
-  let nome = document.getElementById("nome").value;
-  let aulas = parseInt(document.getElementById("aulas").value);
-  let diasIndisponiveis = obterDiasIndisponiveis();
-  adicionarDisciplina(nome, aulas, diasIndisponiveis);
-  formDisciplina.reset();
-});
-
-let botaoReorganizar = document.getElementById("reorganizar");
-botaoReorganizar.addEventListener("click", function(event) {
-  event.preventDefault();
-  reorganizarDisciplinas();
-});
-
-function obterDiasIndisponiveis() {
-  let diasIndisponiveis = [];
-  let checkboxes = document.getElementsByName("dias");
-  checkboxes.forEach(function(checkbox) {
-    if (checkbox.checked) {
-      diasIndisponiveis.push(checkbox.value);
-    }
-  });
-  return diasIndisponiveis;
-}
-
-function adicionarDisciplina(nome, aulas, diasIndisponiveis) {
-  let diasDisponiveis = [...diasSemana];
-  let aulasRestantes = aulas;
-  let corDisciplina = getRandomColor(); // Gera uma cor aleatória para a disciplina
-
-  while (aulasRestantes > 0 && diasDisponiveis.length > 0) {
-    let diaAleatorio = getRandomDia(diasDisponiveis);
-    let periodosDisponiveis = getPeriodosDisponiveis(diaAleatorio, diasIndisponiveis);
-
-    if (periodosDisponiveis.length > 0) {
-      let periodoAleatorio = getRandomPeriodo(periodosDisponiveis);
-      disciplinas[diaAleatorio][periodoAleatorio] = {
-        nome: nome,
-        cor: corDisciplina,
-        diasIndisponiveis: diasIndisponiveis
-      };
-      aulasRestantes--;
-    } else {
-      diasDisponiveis = diasDisponiveis.filter(dia => dia !== diaAleatorio);
-    }
-  }
-
-  atualizarTabela();
-}
-
-function getRandomDia(dias) {
-  let randomIndex = Math.floor(Math.random() * dias.length);
-  return dias[randomIndex];
-}
-
-function getPeriodosDisponiveis(dia, diasIndisponiveis) {
-  let periodos = [];
-  for (let i = 0; i < 5; i++) {
-    if (!disciplinas[dia][i] && !verificarDiaIndisponivel(dia, i, diasIndisponiveis)) {
-      periodos.push(i);
-    }
-  }
-  return periodos;
-}
-
-function verificarDiaIndisponivel(dia, periodo, diasIndisponiveis) {
-  for (let i = 0; i < diasIndisponiveis.length; i++) {
-    if (diasIndisponiveis[i] === dia) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function getRandomPeriodo(periodos) {
-  let randomIndex = Math.floor(Math.random() * periodos.length);
-  return periodos[randomIndex];
-}
-
-function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function atualizarTabela() {
-  let corpoTabela = document.getElementById("corpo-tabela");
-  corpoTabela.innerHTML = "";
+  // Criação das matrizes para controle de status das células
+  const statusMatriz = [];
+  const bloqueioMatriz = [];
 
   for (let i = 0; i < 5; i++) {
-    let linha = document.createElement("tr");
-    let colunaDia = document.createElement("td");
-    colunaDia.textContent = diasSemana[i];
-    linha.appendChild(colunaDia);
-
+    statusMatriz[i] = [];
+    bloqueioMatriz[i] = [];
     for (let j = 0; j < 5; j++) {
-      let colunaPeriodo = document.createElement("td");
-      let disciplina = disciplinas[diasSemana[i]][j];
-      if (disciplina) {
-        colunaPeriodo.textContent = disciplina.nome;
-        colunaPeriodo.style.backgroundColor = disciplina.cor;
-      }
-      linha.appendChild(colunaPeriodo);
-    }
-
-    corpoTabela.appendChild(linha);
-  }
-}
-
-function reorganizarDisciplinas() {
-  let disciplinasArray = [];
-
-  // Converter as disciplinas em um array para facilitar o reajuste
-  for (let dia in disciplinas) {
-    for (let periodo in disciplinas[dia]) {
-      disciplinasArray.push({
-        dia: dia,
-        periodo: periodo,
-        disciplina: disciplinas[dia][periodo]
-      });
+      statusMatriz[i][j] = '';
+      bloqueioMatriz[i][j] = false;
     }
   }
 
-  disciplinasArray.sort((a, b) => b.disciplina.diasIndisponiveis.length - a.disciplina.diasIndisponiveis.length);
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  let index = 0;
-  for (let dia of diasSemana) {
-    disciplinas[dia] = {};
-    let aulasRestantes = disciplinasArray.filter(item => item.dia === dia).length;
+    const nome = document.getElementById('nome').value;
+    const aulas = parseInt(document.getElementById('aulas').value);
 
+    // Verifica se há células suficientes disponíveis para alocar as aulas
+    let celulasDisponiveis = 0;
     for (let i = 0; i < 5; i++) {
-      if (aulasRestantes === 0) {
+      for (let j = 0; j < 5; j++) {
+        if (statusMatriz[i][j] === '') {
+          celulasDisponiveis++;
+        }
+      }
+    }
+
+    if (celulasDisponiveis < aulas) {
+      alert('Não há células suficientes disponíveis para alocar todas as aulas.');
+      return;
+    }
+
+    // Distribui as aulas nas células disponíveis
+    let aulasDistribuidas = 0;
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        if (statusMatriz[i][j] === '') {
+          if (!bloqueioMatriz[i][j]) {
+            statusMatriz[i][j] = nome;
+            aulasDistribuidas++;
+            if (aulasDistribuidas === aulas) {
+              break;
+            }
+          }
+        }
+      }
+      if (aulasDistribuidas === aulas) {
         break;
       }
+    }
 
-      if (!disciplinas[dia][i]) {
-        disciplinas[dia][i] = disciplinasArray[index].disciplina;
-        index++;
-        aulasRestantes--;
+    // Atualiza a tabela com as aulas distribuídas
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        tabela.rows[i + 1].cells[j + 1].textContent = statusMatriz[i][j];
       }
     }
-  }
 
-  atualizarTabela();
-}
+    // Limpa os campos do formulário
+    form.reset();
+  });
 
-// Inicialização da matriz de disciplinas
-for (let i = 0; i < diasSemana.length; i++) {
-  disciplinas[diasSemana[i]] = {};
-}
+  // Atualiza o bloqueio das células quando há alterações nas indisponibilidades
+  const checkboxes = document.querySelectorAll('input[name="indisponibilidades"]');
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+      const value = checkbox.value;
+      const [dia, periodo] = value.split('-');
+      const indiceDia = ['segunda', 'terca', 'quarta', 'quinta', 'sexta'].indexOf(dia);
+      const indicePeriodo = parseInt(periodo);
 
-atualizarTabela();
+      bloqueioMatriz[indiceDia][indicePeriodo] = checkbox.checked;
+
+      // Limpa o conteúdo das células bloqueadas
+      if (checkbox.checked) {
+        if (statusMatriz[indiceDia][indicePeriodo] !== '') {
+          statusMatriz[indiceDia][indicePeriodo] = '';
+          tabela.rows[indiceDia + 1].cells[indicePeriodo + 1].textContent = '';
+        }
+      }
+    });
+  });
+});
